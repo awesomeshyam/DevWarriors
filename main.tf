73930f0f2c1b634cc0e821dc41e3cca0d055bc92
NISHU <awesomeshyam91@gmail.com>
2024-12-17 21:25:28 +0530

    rembo rg added

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "vijay" {
   name     = "rg-vijay"
   location = "West Europe"
 }
+
+resource "azurerm_resource_group" "rembo" {
+  name     = "rg-rembo"
+  location = "West Europe"
+}
